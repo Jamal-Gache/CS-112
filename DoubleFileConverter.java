18069ce3783f3590e50b7364cd04f0431a1542c4
Jamal <jamalmark2014@gmail.com>
2025-06-10 18:32:15 +0000

    Add files via upload

---
+++ DoubleFileConverter.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class DoubleFileConverter {
+
+	private static ArrayList<Double> readBinaryFile(String doubleBinaryFilename){
+		 
+		
+	};
+	
+	private static ArrayList<Double> readBinaryListFile(String doubleBinaryFilename){
+		 
+		
+	};
+	
+	private static ArrayList<Double> readTextFile(String doubleBinaryFilename){
+		 
+		
+	};
+	
+	private static void writeBinaryFile(String doubleBinaryFilename, ArrayList<Double> doubles) {
+		
+	}
+}
